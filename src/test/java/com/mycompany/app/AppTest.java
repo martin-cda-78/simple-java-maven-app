e8a51b3f0dc4aab1a13f7618a780e9489d11d0e9
martin <61967915+martin-cda-78@users.noreply.github.com>
2025-03-11 11:18:44 +0100

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,14 +1,10 @@
 package com.mycompany.app;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
     @Test
     public void testAppConstructor() {
         App app1 = new App();
@@ -17,8 +13,7 @@ public class AppTest
     }
 
     @Test
-    public void testAppMessage()
-    {
+    public void testAppMessage()  {
         App app = new App();
         assertEquals("Hello World!", app.getMessage());
     }
